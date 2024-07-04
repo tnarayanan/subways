312da0df0f9b8d6191c019bcd3ba0f29a198cbd5
Tejas Narayanan <tejasn100@gmail.com>
2024-07-04 17:56:20 -0400

    Initial Commit

---
+++ Subways/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Subways
+//
+//  Created by Tejas Narayanan on 7/4/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Subways/SubwaysApp.swift
@@ -0,0 +1,32 @@
+//
+//  SubwaysApp.swift
+//  Subways
+//
+//  Created by Tejas Narayanan on 7/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SubwaysApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
